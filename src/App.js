acba039649a2a9464ec9d4013e3d183a78f484bf
Dev1000 <javaimagination@gmail.com>
2020-06-05 01:52:40 -0500

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi Lalo! como estas ANGELINITA DE MI CORAZON
+          Hi Lalo! como estas ANGELINITA DE MI CORAZON, soy tu papa precioso! :)
         </p>
         <a
           className="App-link"
